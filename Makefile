c3007a3ca2824c94b576d1ee7b4213ff71f208d5
Troy Sankey <sankeytms@gmail.com>
2022-02-07 10:18:06 -0800

    crashes include data up to 2020, and now include youngest victim age

--- Makefile
+++ Makefile
@@ -2,8 +2,12 @@
 .DEFAULT_GOAL := local
 .PHONY : requirements fetch-osm-data
 
+update-requirments :
+	pip install -r requirements-pip.txt
+	pip-compile requirements.in
 requirements : 
 	npm ci
+	pip install -r requirements.txt
 fetch-osm-data : layers/current_bike_infrastructure.geojson layers/schools.geojson layers/sexy_streets.geojson layers/crashes.geojson
 local : fetch-osm-data requirements
 	python3 -m http.server
@@ -13,14 +17,13 @@ layers/current_bike_infrastructure.geojson : layers/current_bike_infrastructure.
 layers/current_bike_infrastructure.osm : queries/current_bike_infrastructure.osm
 	wget -O $@ --post-file=$< "https://overpass-api.de/api/interpreter"
 
-
 layers/schools.geojson : layers/schools.osm
 	node_modules/osmtogeojson/osmtogeojson $< >$@
 layers/schools.osm : queries/schools.osm
 	wget -O $@ --post-file=$< "https://overpass-api.de/api/interpreter"
 
-layers/sexy_streets.geojson : gis/San\ Diego\ Sexy\ Streets\ Projects\ FY22-23.kml
+layers/sexy_streets.geojson : raw_data/San\ Diego\ Sexy\ Streets\ Projects\ FY22-23.kml
 	node_modules/@mapbox/togeojson/togeojson "$<" >$@
 
-layers/crashes.geojson : gis/Crashes-sd-county-2011-2018.csv
-	node_modules/csv2geojson/csv2geojson --lon POINT_X --lat POINT_Y "$<" >$@
+layers/crashes.geojson : raw_data/TIMS/Crashes.csv raw_data/TIMS/Victims.csv
+	gis/crashes.py
---
+++ gis/crashes.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point
+
+crashes_df = pd.read_csv("raw_data/TIMS/Crashes.csv")
+victims_df = pd.read_csv("raw_data/TIMS/Victims.csv")
+
+# Narrow down the crashes columns to include only fields shown on the map
+crashes_df = crashes_df[
+    [
+        "CASE_ID",
+        "COLLISION_DATE",
+        "COLLISION_SEVERITY",
+        "POINT_X",
+        "POINT_Y",
+    ]
+]
+
+# Narrow down the victims columns to include only fields shown on the map
+victims_df = victims_df[
+    [
+        "CASE_ID",
+        "VICTIM_AGE",
+        "VICTIM_ROLE",
+    ]
+]
+
+# Vicim roles 3 and 4 mean "Pedestrian" and "Bicyclist" respectively.
+victims_df = victims_df[(victims_df["VICTIM_ROLE"] == 3) | (victims_df["VICTIM_ROLE"] == 4)]
+victims_df["VICTIM_ROLE"] = victims_df["VICTIM_ROLE"].map({3: "pedestrian", 4: "bicyclist"})
+
+# Narrow down the victims records to include the youngest one for a given crash.
+victims_df = victims_df[victims_df.groupby(["CASE_ID"])["VICTIM_AGE"].transform(min) == victims_df["VICTIM_AGE"]]
+victims_df = victims_df[["CASE_ID", "VICTIM_AGE", "VICTIM_ROLE"]]
+
+# Join victims into crashes
+crashes_df = crashes_df.merge(victims_df, how='left')
+
+# Convert cleaned and filtered crash data into a geopandas dataframe
+crashes_geometry = [Point(xy) for xy in zip(crashes_df["POINT_X"], crashes_df["POINT_Y"])]
+crashes_df = crashes_df.drop(["POINT_X", "POINT_Y"], axis=1)
+crashes_gdf = gpd.GeoDataFrame(crashes_df, crs="EPSG:4326", geometry=crashes_geometry)
+
+# Write out geojson
+crashes_gdf.to_file("layers/crashes.geojson", driver="GeoJSON")
